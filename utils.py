31db8b03f79fc38845d5c75b087fe55c2503e139
qtpython <qtkhajacloud@gmail.com>
2021-08-15 16:33:31 +0530

    Added is prime

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+def is_prime(number):
+    for index in range(2, number//2):
+        if number%index == 0:
+            return False
+    return True
+
 def sum_of_prime_numbers(max):
     numbers = list(range(2, max+1))
-    return sum(numbers)
+    prime_numbers = list(filter(is_prime, numbers))
+    return sum(prime_numbers)
