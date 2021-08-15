410e4846dd271865caf9d939add8e2bf3232e943
qtpython <qtkhajacloud@gmail.com>
2021-08-15 16:43:19 +0530

    Added date time

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ inventory application
 
 This module will have methods to display commandline options and menus
 """
+import datetime
+
 def application_version():
     return "v1.0"
     
 def menu():
     pass
 
+def main():
+    print(f"Welcome to inventory {datetime.datetime.now()}")
 
 if __name__ == "__main__":
     print("Welcome to Inventory")
